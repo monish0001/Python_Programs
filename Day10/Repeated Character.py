f32187db906569413e3e6e637bc2415d7442331b
Mohd Monish <32845817+monish0001@users.noreply.github.com>
2026-02-17 14:06:51 +0530

    Non Repeating Character and Repeated Character

---
+++ Day10/Repeated Character.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstRep(self, s):
+        # code here
+        dict={}
+        for ch in s:
+            if ch in dict:
+                dict[ch]+=1
+            else:
+                dict[ch]=1
+            
+        for ch in s:
+            if dict[ch]>1:
+                return ch
+            
+        
+        return '#'
